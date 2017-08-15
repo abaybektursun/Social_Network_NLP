1d88fdb8b10619bd272b2a14b77f71d9b69c6672
abaybektursun <abaybektursun@gmail.com>
2017-08-15 03:51:56 +0000

    sync
    Former-commit-id: 44d768a8a9a36336f8fd03d5d15d388293753515

--- word_embeddings/data_massage.py
+++ word_embeddings/data_massage.py
@@ -28,20 +28,37 @@ comment['from'].get('name',''),
 comment['from']['id'],
 comment['message']
 """
+#popular-both-first.txt
+#popular-last.txt
+
+common_words = []
+
+with open('popular-both-first.txt') as openfile:
+    common_words += openfile.read().split("\n")
+with open('popular-last.txt') as openfile:
+    common_words += openfile.read().split("\n")
+
+common_words += ['which','when','can','time','into','some','them','other','than','then','also','back','two','how','first','way','even','because','any','these','day','most','all','just','not','as','up','there','the','a','an','and','or','if','that','so','this','year','one','our','his','her','my','their','your','its','to','of','in','for','on','with','at','by','from','out','over','after','but','about','I','it','he','you','they','we','she','me','him','us','what','who','be','have','would','get','could','see','look','come','give','say','do','will','go','make','know','take','use','work']
+
+common_words = [x.lower() for x in common_words]
 
 in_str   = sys.argv[1]
 page_ids = in_str.split(',')
 
+
+def remove_common_words(string): 
+    return ' '.join([item for item in string.lower().split(' ') if item not in common_words]) + ' '
 out_name = 'data.txt'
 with open(out_name, 'w') as out_file:
     for page_id in page_ids:
         csv_name          = '../fb_scrapper/{}_page.csv'.format(page_id)
         csv_comments_name = '../fb_scrapper/{}_page_comments.csv'.format(page_id)
         with open(csv_name)          as csv_file, \
-             open(csv_comments_name) as comments_csv_file
+             open(csv_comments_name) as comments_csv_file:
+            
             reader = csv.reader(csv_file)
             comment_reader = csv.reader(comments_csv_file)
             for row in reader:
-                out_file.write(re.sub("[^a-zA-Z]+"," ",row[3])+' ')
+                out_file.write( remove_common_words(re.sub("[^a-zA-Z]+"," ",row[3])) )
             for row in comment_reader:
-                out_file.write(re.sub("[^a-zA-Z]+"," ",row[5])+' ')
+                out_file.write( remove_common_words(re.sub("[^a-zA-Z]+"," ",row[5])) )
