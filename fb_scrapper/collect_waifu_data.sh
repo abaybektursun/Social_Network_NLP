8e16de151fbb278feb33253b961db045d2446d19
Jonathan Spikes <jonathan.spikes@wachter.com>
2017-08-17 12:55:57 -0500

    bash job
    Former-commit-id: 70742e2ddbab9e0b366052675bf41f06d1059c80

--- fb_scrapper/collect_waifu_data.sh
+++ fb_scrapper/collect_waifu_data.sh
@@ -1,6 +1,23 @@
-PAGES=(WeAreKWGB KDNAnime ReWaifuProject)
+PAGES=(580878808676318 SwaguChanBasedAnime Anime.Otaku.Fans.Rocks Miriaharven 219980734820071 FluffsBizarreAdventure)
 
-for i in ${PAGES[*]}
+#add 50 page, run 2 pages same time
+pids=""
+for ((i = 0; i < ${#PAGES[@]}; i++))
 do
-    nohup python3 scrap.py $i &
+    #nohup python3 scrap.py $i &
+    #nohup echo ${PAGES[i]} &
+    #nohup echo ${PAGES[i+1]} &
+    nohup python scrap.py ${PAGES[i]} &
+    pids+=" $!"
+    nohup python scrap.py ${PAGES[i+1]} &
+    pids+=" $!"
+    nohup python scrap.py ${PAGES[i+2]} &
+    pids+=" $!"
+    let "i++"
+    let "i++"
+    wait $pids || { echo "there were errors" >&2; }
+    echo "next"
 done
+
+
+
