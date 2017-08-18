ccbed56afedadf5a49c21160e61b644e5e2709e0
abaybektursun <abaybektursun@gmail.com>
2017-08-17 20:24:16 -0500

    sync
    Former-commit-id: bd4f46168b1cc3d1c2aa557eb6e4cb7dde7beead

--- fb_scrapper/collect_waifu_data.sh
+++ fb_scrapper/collect_waifu_data.sh
@@ -1,17 +1,17 @@
-PAGES=(580878808676318 SwaguChanBasedAnime Anime.Otaku.Fans.Rocks Miriaharven 219980734820071 FluffsBizarreAdventure)
-
-#add 50 page, run 2 pages same time
+PAGES=(ReWaifuProject HTNOCMisheardLyrics WaifuWorks LewdShips 1610788579132690 waifuspaceprogram SenpaiWaifu69 KiyoshiSugoi AdrianoSempai.LoliWaifu loliwaifus ILoveYouOniiChan Squiddiesgunnagetya lewds.are.sin.1 lewdwaifus4u VeryLewdLife ThisIsNotALewdPage 1410987655779380 553359274700177 217055368736093 thatsprettyillegal)
+ 
+#add 50 page run 2 pages same time
 pids=""
 for ((i = 0; i < ${#PAGES[@]}; i++))
 do
     #nohup python3 scrap.py $i &
     #nohup echo ${PAGES[i]} &
     #nohup echo ${PAGES[i+1]} &
-    nohup python scrap.py ${PAGES[i]} &
+    nohup python3 scrap.py ${PAGES[i]} &
     pids+=" $!"
-    nohup python scrap.py ${PAGES[i+1]} &
+    nohup python3 scrap.py ${PAGES[i+1]} &
     pids+=" $!"
-    nohup python scrap.py ${PAGES[i+2]} &
+    nohup python3 scrap.py ${PAGES[i+2]} &
     pids+=" $!"
     let "i++"
     let "i++"
