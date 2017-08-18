51e374e2dbc8a9519e18be5cc3157d191a2630ef
abaybektursun <abaybektursun@gmail.com>
2017-08-18 10:17:31 -0500

    sync
    Former-commit-id: 4c1484270e3d7ec48dcf84b6dbaa2f6140adb686

--- word_embeddings/data_massage.py
+++ word_embeddings/data_massage.py
@@ -1,35 +1,8 @@
+import glob
 import sys
 import csv
 import re
 
-"""
-post['id'],
-post.get('from','')['id'],
-post.get('from','')['name'],
-post.get('message',''),
-post.get('link', ''),
-post.get('tags',''),
-post.get('object_attachment',''),
-post['created_time'],
-post.get('description',''),
-post['reactions_count']['reactions_like'    ]['summary']['total_count'],
-post['reactions_count']['reactions_love'    ]['summary']['total_count'],
-post['reactions_count']['reactions_wow'     ]['summary']['total_count'],
-post['reactions_count']['reactions_haha'    ]['summary']['total_count'],
-post['reactions_count']['reactions_sad'     ]['summary']['total_count'],
-post['reactions_count']['reactions_angry'   ]['summary']['total_count'],
-post['reactions_count']['reactions_thankful']['summary']['total_count']
-
-
-post['id'],
-comment['id'],
-comment['created_time'],
-comment['from'].get('name',''),
-comment['from']['id'],
-comment['message']
-"""
-#popular-both-first.txt
-#popular-last.txt
 
 common_words = []
 
@@ -40,14 +13,22 @@ with open('popular-last.txt') as openfile:
 
 common_words += ['which','when','can','time','into','some','them','other','than','then','also','back','two','how','first','way','even','because','any','these','day','most','all','just','not','as','up','there','the','a','an','and','or','if','that','so','this','year','one','our','his','her','my','their','your','its','to','of','in','for','on','with','at','by','from','out','over','after','but','about','I','it','he','you','they','we','she','me','him','us','what','who','be','have','would','get','could','see','look','come','give','say','do','will','go','make','know','take','use','work']
 
-common_words = [x.lower() for x in common_words]
 
-in_str   = sys.argv[1]
-page_ids = in_str.split(',')
+common_words = [x.lower() for x in common_words]
+glob.glob("../fb_scrapper/data/*_page.csv")
+
+#in_str   = sys.argv[1]
+#page_ids = in_str.split(',')
+
+page_ids = []
+for a_path in glob.glob("../fb_scrapper/data/*_page.csv"):
+    a_name = a_path.split('/')[-1]
+    page_ids.append(a_name.split('_')[0])
 
 
 def remove_common_words(string): 
     return ' '.join([item for item in string.lower().split(' ') if item not in common_words]) + ' '
+
 out_name = 'data.txt'
 with open(out_name, 'w') as out_file:
     for page_id in page_ids:
