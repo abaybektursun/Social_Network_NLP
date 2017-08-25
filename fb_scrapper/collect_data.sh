6e474e44fcb0c9b1447227b83c56625cdb6f6d66
abaybektursun <abaybektursun@gmail.com>
2017-08-24 22:28:32 -0500

    uhhhh

--- fb_scrapper/collect_data.sh
+++ fb_scrapper/collect_data.sh
@@ -7,11 +7,11 @@ do
     #nohup python3 scrap.py $i &
     #nohup echo ${PAGES[i]} &
     #nohup echo ${PAGES[i+1]} &
-    nohup python3 scrap.py ${PAGES[i]} &
+    nohup python3 scrap.py ${PAGES[i]} $TOPIC &
     pids+=" $!"
-    nohup python3 scrap.py ${PAGES[i+1]} &
+    nohup python3 scrap.py ${PAGES[i+1]} $TOPIC &
     pids+=" $!"
-    nohup python3 scrap.py ${PAGES[i+2]} &
+    nohup python3 scrap.py ${PAGES[i+2]} $TOPIC &
     pids+=" $!"
     let "i++"
     let "i++"
