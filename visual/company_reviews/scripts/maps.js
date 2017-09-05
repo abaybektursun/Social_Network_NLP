d18ab39325ecaf5591ad267964fa282118b13dff
abaybektursun <abaybektursun@gmail.com>
2017-09-04 23:55:16 -0500

    symc

--- visual/company_reviews/scripts/maps.js
+++ visual/company_reviews/scripts/maps.js
@@ -7,19 +7,25 @@ function tooltipHtml(n, d){	/* function to create html content string in tooltip
 		"</table>";
 }
 
-var sampleData ={};	/* Sample random data. */	
+var sampleData ={};	/* Sample random data. */
+
 ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
 "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
 "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
 "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
 "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
-	.forEach(function(d){ 
-		var low=Math.round(100*Math.random()), 
-			mid=Math.round(100*Math.random()), 
-			high=Math.round(100*Math.random());
-		sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-				avg:Math.round((low+mid+high)/3), color:d3.interpolate("#ffffcc", "#800026")(low/100)}; 
-	});
+.forEach(
+    function(d){ 
+	    var low=Math.round(100*Math.random()), 
+	    	mid=Math.round(100*Math.random()), 
+	    	high=Math.round(100*Math.random());
+	    sampleData[d]={
+            low:d3.min([low,mid,high]), 
+            high:d3.max([low,mid,high]), 
+	        avg:Math.round((low+mid+high)/3), color:d3.interpolate("#ffffcc", "#800026")(low/100)
+        }; 
+    }
+);
 
 /* draw states on id #statesvg */	
 uStates.draw("#statesvg", sampleData, tooltipHtml);
