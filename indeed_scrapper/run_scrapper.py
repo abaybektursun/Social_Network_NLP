9252cc9a11d5bbda0b6f97768882b0b3dde21e40
abaybektursun <abaybektursun@gmail.com>
2017-09-09 17:54:41 -0500

    sync

--- indeed_scrapper/run_scrapper.py
+++ indeed_scrapper/run_scrapper.py
@@ -1,12 +1,5 @@
 from indeed_scrapper import run
 
-run("Xerox")
-run("HP")
-run("Accenture")
-run("Motorola-Solutions")
-run("Cognizant-Technology-Solutions")
-run("Hitachi")
-run("Infosys")
-run("Amadeus-IT-Group-Sa")
+run("Wipro")
 
 
